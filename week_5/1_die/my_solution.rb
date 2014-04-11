785b98beb99a25a8e493985b4734e4bcda964f7a
nataliemac81 <nataliemccroy@gmail.com>
2014-04-10 23:11:35 -0500

    Update my_solution.rb

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -1,35 +1,41 @@
 # U2.W5: Die Class 1: Numeric
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: an integer from 1 to 6
+# Output: the number of Die sides and result of the roll method
 # Steps:
+# 1. Create a class called Die.
+# 2. Pass the initialize method to the sides variable.
+# 3. Raise an ArgumentError if the integer is less than 1.
+# 4. Define method called sides.
+# 5. Define method called roll.
 
 
 # 3. Initial Solution
 
 class Die
   def initialize(sides)
-    # code goes here
+    @sides = sides
+    raise ArgumentError.new('Something/s wrong! Number should be greater than 1.') if @sides < 1
   end
   
   def sides
-    # code goes here
+    @sides
   end
   
   def roll
-    # code goes here
+    print rand(1..@sides)
   end
 end
 
 
 
 # 4. Refactored Solution
-
+# I couldn't think of a way to simplify the initial code.
 
 
 
@@ -37,9 +43,41 @@ end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+my_die = Die.new(6)
+
+print my_die.sides == 6
+print my_die.respond_to?('sides', 'roll')
+print my_die.roll == (1..6)
 
 
 
 
 
-# 5. Reflection 
+
+# 5. Reflection
+#What parts of your strategy worked? What problems did you face?
+# I initially tried to follow the usual driver test/pseudocode then initial code strategy and I couldn't really figure things out so I
+# started figuring out the initial code first and went backwards to pseudocode. For some reason, I just couldn't
+# wrap my mind around it the first way.
+
+
+#What questions did you have while coding? What resources did you find to help you answer them?
+# I used Ruby docs to research ArgumentError and rand
+#
+
+#Did you learn any new skills or tricks?
+# yeah, I learned the respond_to? method!
+#
+#
+#How confident are you with each of the learning objectives?
+# feeling pretty confident with the learning objectives
+
+
+#Which parts of the challenge did you enjoy?
+# I actually enjoyed writing the driver code this time! Go figure.
+#
+#
+#Which parts of the challenge did you find tedious?
+# none!
+#
+#
